5a7cc1eb021afff6752d41e3f0ea3b53160de6c2
pierrepoulouin <pierrepoulouin@gmail.com>
2019-09-11 14:02:11 +0200

    initialisation

---
+++ esportdata_API/src/main/java/com/simplon/esportdata/EsportdataApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.esportdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportdataApplication.class, args);
+	}
+
+}
