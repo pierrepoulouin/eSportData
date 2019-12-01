b9d6f68e97ad0ea5a782a3eb51e34f6e77bf6c5c
pierrepoulouin <pierrepoulouin@gmail.com>
2019-12-01 16:53:37 +0100

    initiate project front

--- esportdata_API/src/main/java/com/simplon/esportdata/entities/Login.java
+++ esportdata_API/src/main/java/com/simplon/esportdata/entities/Login.java
@@ -1,4 +1,10 @@
 package com.simplon.esportdata.entities;
 
-public class Login {
+import javax.persistence.Entity;
+
+@Entity
+public class Login extends AbstractEntity {
+  private String username;
+  private String password;
+
 }
