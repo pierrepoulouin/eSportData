cdff05b9abfae6f3e3f1b344cf08fda3b83bceb0
Pierre <pierrepoulouin@gmail.com>
2019-12-09 14:40:34 +0100

    create entity login

--- esportdata_API/src/main/java/com/simplon/esportdata/entities/Login.java
+++ esportdata_API/src/main/java/com/simplon/esportdata/entities/Login.java
@@ -1,10 +1,15 @@
 package com.simplon.esportdata.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "t_login")
 public class Login extends AbstractEntity {
+  @Column(nullable = false)
   private String username;
+  @Column(nullable = false)
   private String password;
 
 }
