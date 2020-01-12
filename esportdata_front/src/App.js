6243806c852ec540cae39797ec50837d2b7ccd8a
Pierre <pierrepoulouin@gmail.com>
2020-01-12 16:49:31 +0100

    Update wireframes

--- esportdata_front/src/App.js
+++ esportdata_front/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./js/components/header";
 import Tournament from "./js/pages/tournament.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Tournament />
-      </header>
+      <Header />
+      <Tournament />
     </div>
   );
 }
